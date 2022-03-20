c71a6db189b300795bc3c01fecfcc87e058ddac1
nodejshobby@gmail.com <nodejshobby@gmail.com>
2022-03-20 12:23:53 +0100

    Add dotenv library

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const ytdl = require("ytdl-core");
 const cors = require("cors");
@@ -5,7 +6,7 @@ const path = require("path");
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.static(path.join(__dirname)));

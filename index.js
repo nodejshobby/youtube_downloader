e118da66c0034ce024ded7ad2dfa3bf4580e4c98
nodejshobby@gmail.com <nodejshobby@gmail.com>
2022-03-20 12:14:11 +0100

    set the api url path

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const ytdl = require("ytdl-core");
 const cors = require("cors");
 const path = require("path");
-const fs = require("fs");
 
 const app = express();
 

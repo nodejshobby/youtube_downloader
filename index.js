192fa46558ddc15d0f18ada26cda4258c4ff18d0
nodejshobby@gmail.com <nodejshobby@gmail.com>
2022-03-20 09:16:59 +0100

    add response message

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const app = express();
 const PORT = 3000;
 
 app.use(cors());
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
@@ -17,7 +19,11 @@ app.get("/", (req, res) => {
 
 app.post("/youtube", (req, res) => {
   const { url } = req.body;
+  console.log(url);
   ytdl(url).pipe(fs.createWriteStream("video.mp4"));
+  res.json({
+    status: "Ok",
+  });
 });
 
 app.listen(PORT, () => {
